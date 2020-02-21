f3e842ae90f54f4e235a12019f9ef88a935ea798
Tommie Carey <tommiecarey17@gmail.com>
2020-02-21 23:54:13 +0000

    got rid of more legacy code, removed dependency on perspective.lua, made the game more fun
    Co-Authored-By: Benjamin Setterfield <BenPSetterfield@users.noreply.github.com>

--- main.lua
+++ main.lua
@@ -1,7 +1,6 @@
 
 --------------------------------------------------------------------------------
--- include Corona's "physics" library and Perspective.lua
---------------------------------------------------------------------------------
+-- include Corona's "physics" library --------------------------------------------------------------------------------
 
 --physics stuff
 local physics = require "physics"
@@ -9,7 +8,7 @@ physics.start()
 physics.pause()
 physics.setGravity( 0, 22)
 physics.setScale( 80 )
-physics.setDrawMode( "normal" )
+physics.setDrawMode( "hybrid" )
 math.randomseed(os.time( ))
 
 
@@ -20,7 +19,6 @@ local power = 0
 local tapTimer
 local foodEaten = 0
 local foodTable = {"food1", "food2", "food3"}
-local randNum = math.random(1920)
 local backGroup = display.newGroup()
 local mainGroup = display.newGroup()
 local uiGroup = display.newGroup()
@@ -34,84 +32,57 @@ display.setDefault( "textureWrapX", "repeat" )
 display.setDefault( "textureWrapY", "repeat" )
 
 local x, y = display.contentCenterX, display.contentCenterY
-local o = display.newRect( x, y, 9999999, 9999999 )
-o.fill = { type = "image", filename = "images/background2.png" }
-o.fill.scaleX = 0.00025
-o.fill.scaleY = 0.000138
+local cam = display.newRect( x, y, 9999999, 9999999 )
+cam.fill = { type = "image", filename = "images/background2.png" }
+cam.fill.scaleX = 0.00025
+cam.fill.scaleY = 0.000138
 
 --------------------------------------------------------------------------------
 -- Camera stuff
 --------------------------------------------------------------------------------
 
---local require = require
+--camera scrolling effect)
+local camera = display.newGroup();
+camera.x = 0
+camera:insert( cam )
 
---local perspective = require("perspective")
-
---local function forcesByAngle(totalForce, angle) local forces = {} local radians = -math.rad(angle) forces.x = math.cos(radians) * totalForce forces.y = math.sin(radians) * totalForce return forces end
---local camera = perspective.createView()
-
-
-
--- Create master display group (for global "camera" scrolling effect)
-local game = display.newGroup();
-game.x = 0
-game:insert( o )
-------------------------------------------------------------
 -- X-Axis looping background
-------------------------------------------------------------
 bgDistanceX = 1080
 bgDistanceY = 700
 for i = 1, 1000 do
   sky = display.newImage( "images/background1.png", bgDistanceX, 700, true )
   bgDistanceX = bgDistanceX + 2365
-  game:insert( sky )
+  camera:insert( sky )
 end
 
-
-
 local grass = display.newImage( "images/food2.png", 160, 440, true )
-game:insert( grass )
-
+camera:insert( grass )
 physics.addBody( grass, "static", { friction = 0.5, bounce = 0.3 } )
 
-local grass2 = display.newImage( "images/food2.png", 1120, 440, true )
-game:insert( grass2 )
-
-physics.addBody( grass2, "static", { friction = 0.5, bounce = 0.3 } )
-
-local arrow = display.newImage( "images/food3.png", 50, 120 )
-game:insert( arrow );
-
--- Camera follows bolder automatically
+--------------------------------------------------------------------------------
+--Catball
+--------------------------------------------------------------------------------
+-- Camera follows cat automatically
 local function moveCamera()
-  if (cat.x > 80 or cat.y > 0 )then
-    game.x = -cat.x + 250
-    game.y = -cat.y + 800
+  if (cat.x > 0)then
+    camera.x = -cat.x + 300
+    camera.y = -cat.y + 500
   end
 end
 Runtime:addEventListener( "enterFrame", moveCamera )
 
-
---Background stuff
-
---local background = display.newImageRect( backGroup, "images/background1.png", 1940, 1080 )
---background.X = display.contentCenterX
---background.Y = display.contentCenterY
---background.anchorX = 0
---background.anchorY = 0
-
 --adds a circle and skins a cat onto it
-cat = display.newImage( mainGroup, "images/cat.png", 500, 500 ) cat:scale( 0.2, 0.2)
+cat = display.newImage( mainGroup, "images/cat.png", 500, 500 ) cat:scale( 0.15, 0.15)
 cat.bodyType = "kinematic"
-game:insert( cat )
-
 cat.x = display.actualContentWidth - 1500
-cat.y = display.actualContentHeight - 200
+cat.y = display.actualContentHeight - 124
+camera:insert( cat )
+
 --adds physics to Catball and gives him circle physics.
-physics.addBody( cat, { radius = 85, density = 1, friction = 0.5, bounce = .6} )
+physics.addBody( cat, { radius = 72, density = 1, friction = 0.5, bounce = .6} )
 cat.myName = "Catball"
-cat.linearDamping = .5
-cat.angularDamping = .2
+cat.linearDamping = .35
+cat.angularDamping = .05
 
 --makes the cat draggable, pauses physics while being dragged
 function cat:touch( event )
@@ -128,8 +99,6 @@ function cat:touch( event )
   end
   return true
 end
-
--- add the event listener to the circle
 cat:addEventListener( "touch", cat )
 
 --tracks Catball's position at all times.
@@ -139,11 +108,9 @@ function onEnterFrame( event )
 
   --ui calculations--
   totalDistance = math.round(catballX / 100) - 4
-
-  --ui calculations end--
 end
 Runtime:addEventListener( "enterFrame", onEnterFrame)
-local enemiesDefeated = 0
+
 
 --this rotates the cat and shoots him to the right with increasing strength the more taps have occurred
 local function rotatecat()
@@ -151,7 +118,7 @@ local function rotatecat()
   local tapText = tapCount
   cat:applyForce(power, - power, cat.x, cat.y)
   cat:applyAngularImpulse(500)
-  power = power + math.round(tapCount / 2 + totalDistance / 1000)
+  power = power + math.round(tapCount + totalDistance / 1000) + foodEaten*100 + enemiesDefeated*10
 end
 Runtime:addEventListener( "tap", rotatecat)
 
@@ -161,61 +128,55 @@ local function tapperCountdown( event )
 end
 timer.performWithDelay( 5000, tapperCountdown)
 
-
---Adds collision rules to erase food when it contacts Catball, increments foodEaten by 1, intended outcome of Cat hitting food is for him to bounce off to the right at 45 degrees, it doesn't
+--------------------------------------------------------------------------------
+--Collision
+--------------------------------------------------------------------------------
+--Adds collision rules to erase food when it contacts Catball and alter velocity, also removes enemies on contact
+enemiesDefeated = 0
 function onCollision( event )
+
   if (event.phase == "began" ) then
 
     if event.object1.myName == "Catball" and event.object2.myName == "food" then
       foodEaten = foodEaten + 1
       CBx, CBy = event.object1:getLinearVelocity()
-      event.object1:setLinearVelocity( CBx + 1200, - power - 2200)
+      event.object1:setLinearVelocity( CBx+CBy + 1200, -CBy -power - 2200)
       event.contact.isEnabled = false
-      power = power + 10
       event.object2:removeSelf()
       event.object2 = nil
-
     elseif event.object1.myName == "Catball" and event.object2.myName == "enemy" then
       enemiesDefeated = enemiesDefeated + 1
       CBx, CBy = event.object1:getLinearVelocity()
-      event.object1:setLinearVelocity( CBx + 2500, CBy - 5000)
+      event.object1:setLinearVelocity( CBx + 1800, CBy - 3500)
       event.contact.isEnabled = false
-      power = power + 500
       event.object2:removeSelf()
       event.object2 = nil
     end
+
   end
+
 end
 
 Runtime:addEventListener( "collision", onCollision)
 
-
---adds a floor.
+--------------------------------------------------------------------------------
+--Geometry
+--------------------------------------------------------------------------------
 local floor = display.newRect(0, 0, 500000, 50 )
 floor.anchorX = 0
 floor.anchorY = 1
 floor.x, floor.y = 0, 1080
 physics.addBody( floor, "static", { friction = 2.5, shape = floorShape, bounce = 00 } )
-game:insert(floor)
---debug stuff
---adds ceiling
---local ceiling = display.newRect(0, - 6000, 500000, 50 )
---physics.addBody( ceiling, "static", { friction = 1., bounce = 0.5 } )
---adds walls
---left wall
-local wall = display.newRect( 0, 500, 50, 500000 )
+
+local wall = display.newRect( 0, 600, 1, 500000 )
 wall.x, wall.y = 0, 1080
 physics.addBody( wall, "static", { friction = 1.5, bounce = 0.5 } )
---right wall
---local wall2 = display.newRect( 20000, 0, 50, 500000 )
---physics.addBody( wall2, "static", { friction = 1.5, bounce = 0.5 } )
 
+camera:insert(floor)
 
 --------------------------------------------------------------------------------
 -- adds UI elements
 --------------------------------------------------------------------------------
-
-
 foodText = display.newText( uiGroup, "Power: " .. power, 1500, 80, native.systemFont, 36)
 foodText:setFillColor( 0, 0, 0 )
 tapText = display.newText( uiGroup, "Total taps:  " .. tapCount, 1500, 120, native.systemFont, 36 )
@@ -234,62 +195,55 @@ local function updateText()
   distanceText.text = "Total Distance: " .. totalDistance
   totalScore = ((power - tapCount * 5) + (foodEaten * 500) + (totalDistance * 10) / 2)
   scoreText.text = "Score: "..totalScore
-
 end
 Runtime:addEventListener( "enterFrame", updateText)
 
 --------------------------------------------------------------------------------
--- Food Spawns, very messy, needs optimizing
+-- Food Spawns
 --------------------------------------------------------------------------------
-
-
 --foodXSpawn set to 1000 pixels
 local foodXSpawn = 650
 local foodSpacer = 750
-
---food spawned contnuosly every 300 pixels, this continues infinitely but the camera doesn't follow Catball
+--food spawned for 500 of each item over an increasing distance.
 local food = {}
 for i = 1, 500 do
-  local food1 = display.newImage( mainGroup, "images/food1.png", foodXSpawn + foodSpacer * 1.2, 985 ) food1:scale( 0.3, 0.3)
+  local food1 = display.newImage( mainGroup, "images/food1.png") food1:scale( 0.3, 0.3)
   physics.addBody( food1, "static", { radius = 65, density = 0, friction = 1, bounce = 0.5} )
   food1.myName = "food"
+  food1.x = foodXSpawn + foodSpacer * 1.2
+  food1.y = 985
   foodXSpawn = foodXSpawn + 600
-  local food2 = display.newImage( mainGroup, "images/food2.png", foodXSpawn + foodSpacer * 1.6, 960 ) food2:scale( 0.5, 0.5)
+  local food2 = display.newImage( mainGroup, "images/food2.png") food2:scale( 0.5, 0.5)
   physics.addBody( food2, "static", { radius = 90, density = 0, friction = 1, bounce = 0.5} )
   food2.myName = "food"
+  food2.x = foodXSpawn + foodSpacer * 1.6
+  food2.y = 960
   foodXSpawn = foodXSpawn + 600
-  local food3 = display.newImage( mainGroup, "images/food3.png", foodXSpawn + foodSpacer * 1.8, 970 ) food3:scale( 1, 1)
-  physics.addBody( food3, "static", { radius = 65, density = 0, friction = 1, bounce = 0.5} )
+  local food3 = display.newImage( mainGroup, "images/food3.png" ) food3:scale( 1, 1)
+  physics.addBody( food3, "static", { radius = 50, density = 0, friction = 1, bounce = 0.5} )
   food3.myName = "food"
+  food3.x = foodXSpawn + foodSpacer * 1.8
+  food3.y = 970
   foodXSpawn = foodXSpawn + foodSpacer * 1.4
   foodSpacer = foodSpacer * 1.1
-  --camera:add(food1, 4)
-  --camera:add(food2, 4)
-  --camera:add(food3, 4)
-  game:insert(food1)
-  game:insert(food2)
-  game:insert(food3)
+  camera:insert(food1)
+  camera:insert(food2)
+  camera:insert(food3)
 end
-
 
 --------------------------------------------------------------------------------
 -- Enemy Spawns
 --------------------------------------------------------------------------------
-
-
 local enemy = {}
 
-
-for i = 1, 1000 do
+for i = 1, 250 do
   enemy[i] = display.newImage( mainGroup, "images/enemy1.png" ) enemy[i]:scale( 0.5, 0.5)
   physics.addBody( enemy[i], "static", { radius = 50, density = 1, friction = 1, bounce = 2} )
-  enemy[i].x = 5000 + math.random(display.screenOriginX, display.contentWidth * 100)
-  enemy[i].y = -8500 + math.random(display.screenOriginY, display.contentHeight * 7)
+  enemy[i].x = 4000 + math.random(display.screenOriginX, display.contentWidth * 100)
+  enemy[i].y = -7500 + math.random(display.screenOriginY, display.contentHeight * 7)
   enemy[i].myName = "enemy"
-  --camera:add(enemy[i], 4)
-  game:insert(enemy[i])
+  camera:insert(enemy[i])
 end
-
 
 --------------------------------------------------------------------------------
 -- End Level condition and sound test
@@ -328,8 +282,6 @@ Runtime:addEventListener("enterFrame", audioTest)
 --------------------------------------------------------------------------------
 -- Camera stuff
 --------------------------------------------------------------------------------
-
-
 uiGroup:toFront()
 
 
